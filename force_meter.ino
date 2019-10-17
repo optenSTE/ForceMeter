cd6b355fd5c1d46f6e2ffc7f4a3522deecd05cf6
optenSTE <38421767+optenSTE@users.noreply.github.com>
2019-10-17 15:59:13 +0300

    Added force trigger

--- force_meter.ino
+++ force_meter.ino
@@ -1,6 +1,6 @@
 /**********************************************
  *  Force meter sketch for Arduino UNO
- *  shows force value (in kgf)
+ *  shows force value (in kgf) on display
  *  By Andrey Likhobabin andreylikhobabin@ya.ru
  *  2019
 ***********************************************/
@@ -10,7 +10,7 @@
 
 
 /**********************************************
- *  Pinout
+ *  Pinout configuration
 ***********************************************/
 // HX711 - ADC converter https://ru.aliexpress.com/item/33046037411.html?spm=a2g0s.9042311.0.0.5f8a33edzvRB08
 #define HX711_DOUT_PIN 5
@@ -22,19 +22,54 @@
 
 #define TARE_BUTTON_PIN 2 // should be ONLY 2 becasue of interrupt
 
+// Relay for triggering when force in defined limits
+#define RELAY_PIN 13
+
 
 /**********************************************
- *  Global value
+ *  Another configuration
+***********************************************/
+// #define SERIAL_OUTPUT  // enable serial output with force values
+
+// force limits
+#define MAX_FORCE 50
+#define MIN_FORCE -50
+
+// force trigger configuration
+// trigger releses when 
+//    (f_cur > (FORCE_TRIGGER_THRESHOLD - FORCE_TRIGGER_GAP))
+//  and
+//    (f_cur < (FORCE_TRIGGER_THRESHOLD + FORCE_TRIGGER_GAP))
+//  during FORCE_TRIGGER_TIME_MS
+// where f_cur is current averaged force
+#define FORCE_TRIGGER_THRESHOLD 1.5
+#define FORCE_TRIGGER_GAP 0.2
+#define FORCE_TRIGGER_TIME_MS 3000
+
+#define FORCE_TRIGGER_RELAY_HIGH_TIME_MS 1000 // time period for high state of relay when force trigger released
+
+const long SCALE_FACTOR = 23300;          // how many ADC values in one kilo
+const uint8_t AVG_READ_MAX = 1;           // how many ADC outputs will be averaged
+const unsigned long DELAY_MS = 300;       // delays, used in different places
+const uint8_t DISPLAY_BRIGHTNESS = 0xff;  // display brightness
+
+// sumbols for display
+const uint8_t DISPLAY_TARE[] = { SEG_G, SEG_G, SEG_G, SEG_G };
+const uint8_t DISPLAY_ERROR[] = { SEG_A | SEG_F | SEG_E | SEG_D | SEG_G, SEG_G | SEG_E, SEG_G | SEG_E, SEG_G | SEG_E };
+
+
+/**********************************************
+ *  Global variables
 ***********************************************/
 HX711 scale_sensor;
 TM1637Display display(TM1637_CLK_PIN, TM1637_DIO_PIN);
 
 bool tare_pressed = false;
 
-long ADC_tare_value = 0;            // ADC value when tenzo unloaded
-const float scale_factor = 23300.0; // how many ADC values in one kilo
+long adc_tare_value = 0;            // ADC value when tenzo unloaded
+bool display_blink_state_on = true;
 
-// #define SERIAL_OUTPUT  // enable serial output with force values
+unsigned long force_trigger_start = 0;    // time when first force trigger conidions released
 
 
 void setup() 
@@ -42,15 +77,18 @@ void setup()
   pinMode(TARE_BUTTON_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(TARE_BUTTON_PIN), tare_button_func, FALLING);
   
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+  
   #ifdef SERIAL_OUTPUT
     Serial.begin(38400);
   #endif
 
   scale_sensor.begin(HX711_DOUT_PIN, HX711_SCK_PIN);
   scale_sensor.set_gain(64);
-  ADC_tare_value = scale_sensor.read();
+  adc_tare_value = scale_sensor.read();
 
-  display.setBrightness(0x0f, true);
+  display.setBrightness(DISPLAY_BRIGHTNESS, true);
   display.clear();
 
 }
@@ -58,35 +96,92 @@ void setup()
 
 void loop() 
 {
+  bool blink_mode = false;
+  bool scale_sensor_error = false;
+
   // ADC reading
-  const int num_of_meas = 1;
-  long ADC_sum = 0;
-  for (int i=0; i<num_of_meas; i++)
+  long adc_sum = 0;
+  for (int i=0; i<AVG_READ_MAX; i++)
   {
-    ADC_sum += scale_sensor.read();
+    if (scale_sensor.wait_ready_timeout(DELAY_MS))
+    {
+      adc_sum += scale_sensor.read();
+    }
+    else
+    {
+      scale_sensor_error = true;
+      break;
+    }
   }
-  float ADC_mean = ADC_sum/num_of_meas;
+  float adc_avg = adc_sum/AVG_READ_MAX;
 
   // zerroing ADC (pressing Tare button from interrupt)  
   if (tare_pressed)
   {
-    ADC_tare_value = ADC_mean;
+    adc_tare_value = adc_avg;
     tare_pressed = false;
+    
+    // show minuses for a short time
+    display.setSegments(DISPLAY_TARE);
+    delay(DELAY_MS);
   }
   
   // convert ADC to force,kg
-  float force_kg = (ADC_mean - ADC_tare_value) / scale_factor;
-
+  float force_kg = (adc_avg - adc_tare_value) / SCALE_FACTOR;
+  
+  // check conditions for force trigger
+  if ((force_kg > (FORCE_TRIGGER_THRESHOLD - FORCE_TRIGGER_GAP)) and (force_kg < (FORCE_TRIGGER_THRESHOLD + FORCE_TRIGGER_GAP)))
+  {
+    // force is in limit, let's chech time
+    blink_mode = true;
+    if (force_trigger_start == 0)
+      force_trigger_start = millis();
+    else
+    {
+      unsigned long force_trigger_duration = millis() - force_trigger_start; // current time period for force trigger conidions
+      if (force_trigger_duration > FORCE_TRIGGER_TIME_MS)
+      {
+        // trigger is released - let's release relay
+        digitalWrite(RELAY_PIN, HIGH);
+        delay(FORCE_TRIGGER_RELAY_HIGH_TIME_MS);
+        digitalWrite(RELAY_PIN, LOW);
+        force_trigger_start = 0;
+      }
+    }
+  }
+  else
+    force_trigger_start = 0;
+    
+  
   // display force
-  float display_value = force_kg;
-  if (display_value < -50)
-    display_value = -50;
-  if (display_value > 50)
-    display_value = 50;
-
-  int value_x10_int = int(display_value*10);
-  display.showNumberDecEx(value_x10_int, (0x80 >> 2), false);
-
+  if (scale_sensor_error)
+  {
+    display.setSegments(DISPLAY_ERROR);
+    blink_mode = true;
+    delay(DELAY_MS);
+  }
+  else
+  {
+    float display_value = force_kg;
+    
+    if (display_value < MIN_FORCE)
+      display_value = MIN_FORCE;
+    if (display_value > MAX_FORCE)
+      display_value = MAX_FORCE;
+    if ((display_value == MAX_FORCE) or (display_value == MIN_FORCE))
+      blink_mode = true;
+  
+    int value_x10_int = int(display_value*10);
+    display.showNumberDecEx(value_x10_int, (0x80 >> 2), false);
+  }
+  if (blink_mode)
+  {
+    display.setBrightness(DISPLAY_BRIGHTNESS, display_blink_state_on);  // Turn off/on
+    display_blink_state_on = !display_blink_state_on;
+  }
+  else
+    display.setBrightness(DISPLAY_BRIGHTNESS, true);  // Turn on
+  
   #ifdef SERIAL_OUTPUT
     Serial.print(millis()/1000.0, 3);
     Serial.print("\t");
